5c605f56fc720740500947d193bb0655935bffc1
Thibault Saunier <thibault.saunier@osg.samsung.com>
2016-12-07 09:35:35 -0300

    Start implementing a property
    And move to using gtk-rs instead of gi-rust as gi-rust seems rather
    no maintained and using GVallue didn't seem to be possible
    
    Needs https://github.com/gtk-rs/sys/issues/34 to be fixed still.

--- rust-gobject/src/main.rs
+++ rust-gobject/src/main.rs
@@ -15,15 +15,19 @@ mod pointer;
 use self::pointer::Ptr;
 
 #[repr(C)]
+enum TestFooProperties {
+    PROP_FOO_NONE,
+    PROP_FOO_BAR
+}
+
+#[repr(C)]
 struct TestFoo {
     parent_instance: gobject::GObject,
     private: Box<TestFooPrivate>
 }
 
 impl TestFoo {
-    fn private(&self) -> &TestFooPrivate {
-        &self.private
-    }
+    fn private(&self) -> &TestFooPrivate { &self.private }
 }
 
 #[repr(C)]
@@ -38,36 +42,66 @@ struct TestFooClass {
 
 extern "C" fn test_foo_class_init(klass: *mut TestFooClass) {
     unsafe {
-        gobject::g_type_class_add_private(klass as gpointer, mem::size_of::<TestFooPrivate>());
+        gobject::g_type_class_add_private(klass as g::gpointer, mem::size_of::<TestFooPrivate>());
         let g_object_class = klass as *mut gobject::GObjectClass;
-        (*g_object_class).finalize = Some(test_foo_finalize);
+
+        let finalize: gobject::GObjectFinalizeFunc = Some(test_foo_finalize);
+        let set: gobject::GObjectSetPropertyFunc = Some(test_foo_set_property);
+        let get: gobject::GObjectGetPropertyFunc = Some(test_foo_get_property);
+
+        (*g_object_class).finalize = finalize;
+        (*g_object_class).set_property = set;
+        (*g_object_class).get_property = get;
+
+        gobject::g_object_class_install_property (
+            g_object_class, TestFooProperties::PROP_FOO_BAR as u32,
+            gobject::g_param_spec_string (b"foo-bar"  as *const u8 as *const i8,
+                                           b"Foo bar"  as *const u8 as *const i8,
+                                           b"Just a fooish bargy"  as *const u8 as *const i8,
+                                           0 as *const i8,
+                                           gobject::GParamFlags::from_bits(3).unwrap()));
     }
 }
 
-extern "C" fn test_foo_finalize(object: *mut gobject::GObject) {
-    unsafe {
+#[allow(unused_variables)]
+unsafe extern "C" fn test_foo_get_property(object: *mut gobject::GObject,
+                                           property_id: u32,
+                                           value: *mut gobject::GValue,
+                                           pspec: *mut gobject::GParamSpec) {
+    println!("Getting {}", property_id);
+}
+
+#[allow(unused_variables)]
+unsafe extern "C" fn test_foo_set_property(object: *mut gobject::GObject,
+                                    property_id: libc::c_uint,
+                                    value: *const gobject::GValue,
+                                    pspec: *mut gobject::GParamSpec)
+{
+    println!("Setting {}", property_id);
+}
+
+unsafe extern "C" fn test_foo_finalize(object: *mut gobject::GObject) {
         let foo = object as *mut TestFoo;
         mem::drop(&mut (*foo).private);
 
         // FIXME -- g_class field of `GTypeInstance` ought to be `pub`
         let xxx = object as *mut *mut gobject::GTypeClass;
         let object_class = *xxx;
-        let parent_class = gobject::g_type_class_peek_parent(object_class as gpointer);
+        let parent_class = gobject::g_type_class_peek_parent(object_class as g::gpointer);
         let g_object_class = parent_class as *mut gobject::GObjectClass;
         ((*g_object_class).finalize.unwrap())(object);
-    }
 }
 
+#[allow(dead_code)]
 extern "C" fn test_foo_instance_init(obj: *mut TestFoo) {
     let private = Box::new(TestFooPrivate {
         some: Cell::new(5)
     });
-    unsafe {
-        ptr::write(&mut (*obj).private, private);
-    }
+
+    unsafe {ptr::write(&mut (*obj).private, private)};
 }
 
-fn test_foo_construct(object_type: gtypes::GType) -> Ptr<TestFoo> {
+fn test_foo_construct(object_type: g::GType) -> Ptr<TestFoo> {
     unsafe {
         let this: *mut TestFoo = gobject::g_object_new(object_type, ptr::null_mut()) as *mut TestFoo;
         Ptr::new(this)
@@ -79,21 +113,24 @@ fn test_foo_new() -> Ptr<TestFoo> {
 }
 
 lazy_static! {
-    pub static ref TEST_TYPE_FOO: gtypes::GType = {
+    pub static ref TEST_TYPE_FOO: g::GType = {
         unsafe {
             gobject::g_type_register_static_simple(
                 gobject::g_object_get_type(),
-                b"TestFoo\0" as *const u8 as *const i8,
+                "TestFoo".to_glib_none().0,
                 mem::size_of::<TestFooClass>() as u32,
                 mem::transmute(test_foo_class_init as extern "C" fn(*mut TestFooClass)),
                 mem::size_of::<TestFoo>() as u32,
                 mem::transmute(test_foo_instance_init as extern "C" fn(*mut TestFoo)),
                 mem::transmute(0)) // FIXME GTypeFlags should not be an enum
-        }
+       }
     };
 }
 
 fn main() {
     let bar = test_foo_new();
-    println!("Test bar {}", bar)
+
+    bar.set_property(b"foo-bar" as *const u8 as *const i8,
+                     glib::Value::from("HELLO!").to_glib_none().0);
+    println!("Bar is: {}", bar);
 }
--- rust-gobject/src/pointer.rs
+++ rust-gobject/src/pointer.rs
@@ -3,10 +3,8 @@ use std::fmt;
 
 use libc::c_char;
 use std::ffi::CStr;
-use std::str;
 
 use gobject;
-use gtypes;
 
 /// Represents an owned gobject `T`
 pub struct Ptr<T> {
@@ -17,12 +15,19 @@ impl<T> Ptr<T> {
     pub unsafe fn new(data: *mut T) -> Ptr<T> {
         Ptr { data: data }
     }
+
+    pub fn set_property(&self,
+                        prop: *const i8,
+                        value: *const gobject::GValue) {
+        unsafe {gobject::g_object_set_property(self.data as *mut gobject::GObject,
+                                       prop, value)};
+    }
 }
 
 impl<T> Clone for Ptr<T> {
     fn clone(&self) -> Self {
         unsafe {
-            gobject::g_object_ref(self.data as gtypes::gpointer);
+            gobject::g_object_ref(self.data as *mut gobject::GObject);
             Ptr { data: self.data }
         }
     }
@@ -41,16 +46,21 @@ impl<T> Deref for Ptr<T> {
 impl<T> Drop for Ptr<T> {
     fn drop(&mut self) {
         unsafe {
-            gobject::g_object_unref(self.data as gtypes::gpointer);
+            gobject::g_object_unref(self.data as *mut gobject::GObject);
         }
     }
 }
 
 impl<T> fmt::Display for Ptr<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let c_buf: *const c_char = unsafe { gobject::g_type_name_from_instance(self.data as *mut gobject::GTypeInstance) };
+        let c_buf: *const c_char = unsafe {
+            gobject::g_type_name_from_instance(
+                self.data as *mut gobject::GTypeInstance)
+        };
+
         let c_str: &CStr = unsafe { CStr::from_ptr(c_buf)  };
         let typename = c_str.to_str().unwrap();
+
         write!(f, "{}<{:p}>", typename, self.data)
     }
 }
